d66a68ba2e36acd2f6afa59fe4cb046afecb1eac
Nastya07s <nastya-kotovich@mail.ru>
2020-12-29 12:59:30 +0300

    feat: add eslint

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+
 const app = express();
+
 const server = require('http').Server(app);
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-    res.status(200).send('hhhh');
-})
+  res.render('room');
+});
 
 server.listen(3030);
