7d36c52ee74fbd9cb27af761e3fa661610a4d100
Nastya07s <nastya-kotovich@mail.ru>
2020-12-29 13:21:25 +0300

    feat: add room id

--- server.js
+++ server.js
@@ -3,11 +3,16 @@ const express = require('express');
 const app = express();
 
 const server = require('http').Server(app);
+const { v4: uuidv4 } = require('uuid');
 
 app.set('view engine', 'ejs');
 
+app.get('/:room', (req, res) => {
+  res.render('room', { roomId: req.params.room });
+});
+
 app.get('/', (req, res) => {
-  res.render('room');
+  res.redirect(`/${uuidv4()}`);
 });
 
 server.listen(3030);
